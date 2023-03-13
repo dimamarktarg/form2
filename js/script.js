14cf84b92f17bc8665eb708af803969d7024d435
Shevchenko Dmytro <dimamarktarg@gmail.com>
2023-03-13 13:43:40 +0200

    Форма с вводом имени, телефона и сообщения

---
+++ js/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var form = document.getElementById('my-form');
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById('my-form-status');
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then(response => {
+      if (response.ok) {
+        status.innerHTML = 'Спасибо за заявку!';
+        form.reset();
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data['errors']
+              .map(error => error['message'])
+              .join(', ');
+          } else {
+            status.innerHTML = 'Упс! Проблема с отправкой вашей формы';
+          }
+        });
+      }
+    })
+    .catch(error => {
+      status.innerHTML = 'Упс! Проблема с отправкой вашей формы';
+    });
+}
+form.addEventListener('submit', handleSubmit);
